ea11ad30eedeb1aa75d4bca14b3ed2211683abb1
kaushik.galiya <kaushik.galiya@freestoneinfotech.com>
2021-05-17 14:30:11 +0530

    added log and junit mvn

--- practice/src/main/java/MainClass.java
+++ practice/src/main/java/MainClass.java
@@ -1,7 +1,16 @@
+import org.apache.log4j.Logger;
 
+/*
+ * This is the fist class
+ * Author @kaushik29
+ * */
 public class MainClass {
+
+  private static final Logger logger = Logger.getLogger(MainClass.class);
+
   public static void main(String[] args) {
     // just a formally first commit with hello world
-    System.out.println("Hello world");
+    logger.info("Hello world");
+
   }
 }
